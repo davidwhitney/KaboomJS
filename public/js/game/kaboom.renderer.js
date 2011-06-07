7246354f42eba1703614a273dcf212b4b472b1fb
David Whitney <david@davidwhitney.co.uk>
2011-06-07 08:29:38 +0100

    a little broken, moving a load of code around. Wouldn't pull this if I were you.

--- public/js/game/kaboom.renderer.js
+++ public/js/game/kaboom.renderer.js
@@ -1,41 +1,30 @@
 function KaboomRenderer(target, game) {
-	this.itemImages = ["images/solid-block.png", 
-						"images/destroyable-block.png",
-						"images/blank.png" ];
-  this.initialise = function() {
-    var level = game.level;
+
+    this.previousTick = [];
+    this.mapBuilder = new MapBuilder(target, game);
     
-    // create tiles
-    for (var row = 0; row < level.rows.length; row++) {
-    	var rowDiv = $('<div class="row" style="position:absolute;top:'+row*game.TILE_SIZE+'px;height:'+game.TILE_SIZE+'px;width:'+game.TILE_SIZE*level.rows[row].length+'px" />');
+    this.initialise = function() {
+        this.mapBuilder.build(game.level);
+        this.createPlayers();
+    };
 
-    	for (var tileIndex = 0; tileIndex < level.rows[row].length; tileIndex++) {
-    		var tile = level.rows[row][tileIndex];
-    		var tileDiv = $('<div id="tile_' + row + '_' + tileIndex + '" class="tile" style="position:absolute;height:' + game.TILE_SIZE + 'px;width:'+game.TILE_SIZE+'px;top:0;left:'+tileIndex * game.TILE_SIZE+'px" />');
+    this.createPlayers = function() {
+        for (var player = 0; player < game.players.length; player++) {
+            this.createPlayer(player + 1, game.players[player]);
+        }
+    };
 
-    		if (tile != null) {
-  				tileDiv.css('background', 'url(' + this.itemImages[tile.tileType] + ')');
-    		}
-
-    		rowDiv.append(tileDiv);
-    	}
-
-    	target.arena.append(rowDiv);
-    }
-    
-    // create players
-    for (var player = 0; player < game.players.length; player++) {
-      this.createPlayer(player + 1);
-    }
-  };
-  
-  this.createPlayer = function(num) {
+  this.createPlayer = function(num, player) {
     var playerDiv = $('<div id="player_' + num + '" class="player" style="width: 36px;" />');
 
-	playerDiv.sprite({fps: 3, no_of_frames: 3}).active();
+    playerDiv.sprite({fps: 3, no_of_frames: 3}).active();
     playerDiv.spStart();
 
     target.holding.append(playerDiv);
+
+    this.ensureVelocityIsInitialised(player);
+    this.cacheLatestPlayerData(playerDiv, player);
+
   };
 
   this.makePlayerActive = function(playerDiv){
@@ -43,35 +32,45 @@ function KaboomRenderer(target, game) {
       playerDiv.data('isInPlay', true);
   };
 
-  this.playerHasMovedSinceLastDraw = function(playerDiv, serverPlayerState){
+  this.playerHasMovedSinceLastDraw = function(playerDiv, player){
+    var cachedX = playerDiv.data('x');
+    var cachedY = playerDiv.data('y');
+    var cachedDX = playerDiv.data('dx');
+    var cachedDY = playerDiv.data('dy');
+    var direction = this.getPlayerDirectionFromVelocity(player);
 
-    var direction = this.getPlayerDirectionFromVelocity(serverPlayerState);
+    var lastDirection =  playerDiv.data('lastDirection');
+    if(direction !== lastDirection) {
+        return true;
+    }
+    if (cachedY !== player.position.y){
+        return true;
+    }
+    if (cachedX != player.position.x){
+        return true;
+    }
+    if (cachedDX !== player.velocity.dx){
+        return true;
+    }
+    if(cachedDY !== player.velocity.dy){
+        return true;
+    }
 
-    if(direction != playerDiv.data('lastDirection', direction)) {
-        return true;
-    }
-    if (playerDiv.data('y') != serverPlayerState.position.y || playerDiv.data('x') != serverPlayerState.position.x) {
-        return true;
-    }
-    if (playerDiv.data('dx') != serverPlayerState.velocity.dx || playerDiv.data('dy') != serverPlayerState.velocity.dy) {
-        return true;
-    }
     return false;
   };
 
-  this.cacheLatestPlayerData = function(playerDiv, serverPlayerState){
-    playerDiv.data('x', serverPlayerState.position.x);
-    playerDiv.data('y', serverPlayerState.position.y);
-    playerDiv.data('dx', serverPlayerState.velocity.x);
-    playerDiv.data('dy', serverPlayerState.velocity.y);
+this.cacheLatestPlayerData = function(playerDiv, player){
+    playerDiv.data('x', playerDiv.position.x);
+    playerDiv.data('y', playerDiv.position.y);
+    playerDiv.data('dx', player.velocity.x);
+    playerDiv.data('dy', player.velocity.y);
 
-    var direction = this.getPlayerDirectionFromVelocity(serverPlayerState);
+    var direction = this.getPlayerDirectionFromVelocity(player);
     playerDiv.data('lastDirection', direction);
-      
-  };
+};
 
-  this.ensureVelocityIsInitialised = function (player) {
-      if(player.velocity == null){
+  this.ensureVelocityIsInitialised = function(player) {
+      if (player.velocity == "undefined") {
           player.velocity = new Velocity(0,0);
       }
   };
@@ -90,7 +89,7 @@ function KaboomRenderer(target, game) {
         }
   };
 
-   this.updatePlayerSpriteForDirection = function (direction) {
+   this.updatePlayerSpriteForDirection = function(direction) {
         if (direction === "up") {
 
         } else if (direction === "down") {
@@ -104,6 +103,15 @@ function KaboomRenderer(target, game) {
         }
     };
 
+ this.updatePlayerLocations = function() {
+    for (var i = 0; i < game.players.length; i++) {
+        var player = game.players[i];
+        if (player != null) {
+            this.updatePlayer(i, player);
+        }
+    }
+  };
+
  this.updatePlayer = function(playerId, player) {
         var playerDiv = $('#player_' + (playerId + 1));
         if (!playerDiv.data('isInPlay')) {
@@ -114,27 +122,20 @@ function KaboomRenderer(target, game) {
             return;
         }
 
+        //this.ensureVelocityIsInitialised(player);
+
+        var direction = this.getPlayerDirectionFromVelocity(player);
+        this.updatePlayerSpriteForDirection(direction);
+
         playerDiv.css({
             position: 'absolute',
             top: player.position.y + 'px',
             left: player.position.x + 'px'
         });
 
-        this.ensureVelocityIsInitialised(player);
         this.cacheLatestPlayerData(playerDiv, player);
-
-        var direction = this.getPlayerDirectionFromVelocity(player);
-        this.updatePlayerSpriteForDirection(direction);
     };
 
- this.updatePlayerLocations = function() {
-    for (var i = 0; i < game.players.length; i++) {
-        var player = game.players[i];
-        if (player != null) {
-            this.updatePlayer(i, player);
-        }
-    }
-  };
   
   this.updateItems = function() {
     for (var rowIndex = 0; rowIndex < game.level.rows.length; rowIndex++) {
@@ -163,4 +164,31 @@ function KaboomRenderer(target, game) {
   };
   
   this.initialise();
+}
+
+function MapBuilder(target, game){
+
+    this.itemImages = ["images/solid-block.png",
+						"images/destroyable-block.png",
+						"images/blank.png" ];
+
+    this.build = function(level) {
+
+        for (var row = 0; row < level.rows.length; row++) {
+            var rowDiv = $('<div class="row" style="position:absolute;top:' + row * game.TILE_SIZE + 'px;height:' + game.TILE_SIZE + 'px;width:' + game.TILE_SIZE * level.rows[row].length + 'px" />');
+
+            for (var tileIndex = 0; tileIndex < level.rows[row].length; tileIndex++) {
+                var tile = level.rows[row][tileIndex];
+                var tileDiv = $('<div id="tile_' + row + '_' + tileIndex + '" class="tile" style="position:absolute;height:' + game.TILE_SIZE + 'px;width:' + game.TILE_SIZE + 'px;top:0;left:' + tileIndex * game.TILE_SIZE + 'px" />');
+
+                if (tile != null) {
+                    tileDiv.css('background', 'url(' + this.itemImages[tile.tileType] + ')');
+                }
+
+                rowDiv.append(tileDiv);
+            }
+
+            target.arena.append(rowDiv);
+        }
+    };
 }
